aa443aee4cf1ebbede6e2aaf2891bbe22d6fbc6d
윤우섭 <smile-y@yun-useob-ui-MacBookPro.local>
2020-09-25 17:28:31 +0900

    초기설정

---
+++ 001.py
@@ -0,0 +1,14 @@
+"""
+반복문을 사용하여 0부터 n까지의 합 출력하기
+"""
+
+def print_to_n(n):
+    i = 0
+    sum = 0
+    while (i <= n):
+        sum = sum + i
+        i = i + 1
+    print(sum)
+
+if __name__ == "__main__":
+    print_to_n(100)
