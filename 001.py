1e9ebc6bd772c8172a7f23776a05714fbda36dcc
윤우섭 <smile-y@yun-useob-ui-MacBookPro.local>
2020-09-28 17:58:29 +0900

    반복문을 사용하여 0부터 n까지의 합 출력하기

--- 001.py
+++ 001.py
@@ -12,4 +12,10 @@ def print_to_n(n):
 
 if __name__ == "__main__":
     print_to_n(100)
-    
+
+# def print_sum(n):
+
+#     for i in range(print_sum(n)):
+#         print(i)
+# if __name__ == "__mian__":
+#     print_sum(100)
