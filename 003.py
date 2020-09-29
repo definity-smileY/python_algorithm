2eb8236487233e2fac19feb46001232d52c5b87e
윤우섭 <smile-y@yun-useob-ui-MacBookPro.local>
2020-09-29 17:54:07 +0900

    반복문을 사용하여 1부터 n까지 출력하기

---
+++ 003.py
@@ -0,0 +1,16 @@
+"""
+반복문을 사용하여 1부터 n까지 출력하기
+"""
+
+def print_to_n(n):
+    num = 1
+    while n >= num:
+        
+        num = num + 1
+    print(num)
+        
+    
+if __name__=='__main__':
+    print_to_n(19)
+
+    
