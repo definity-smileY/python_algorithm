071a0e3fc9272dec5454b64410829fca4f949ae0
definity-smileY <yysdntjq@naver.com>
2021-06-12 18:01:06 +0900

    upload TODO codeup_basic_100

---
+++ CodeUp_basic_100/12_출력하기.py
@@ -0,0 +1,17 @@
+"""
+본 문제는 python 의 빠른 기초 학습을 위해 설계된 문제로서 python 코드 제출을 기준으로 설명되어 있습니다. 
+------
+
+줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+예시
+a = input() 
+b = input()
+print(a)
+print(b)
+과 같은 방법으로 가능하다.
+"""
+int1 = input()
+int2 = input()
+print(int1)
+print(int2)
