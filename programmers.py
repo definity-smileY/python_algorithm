918f43449b31548868f0e765e961ec09b71b1ac9
definity-smileY <yysdntjq@naver.com>
2021-06-20 21:43:28 +0900

    TODO updated

--- programmers.py
+++ programmers.py
@@ -1,4 +1,21 @@
-num = 3
-print(num % 2)
-num = num * 3
-print(num)
+# def solution(num):
+#     answer = 0    
+#     while num > 1:
+#         if num % 2 == 0:
+#             num = num / 2
+        
+#         if num % 2 == 1:
+#             num = num * 3 + 1
+        
+#         answer += 1
+
+#         if answer >= 500:
+#             return -1
+        
+#     return answer
+        
+# solution(6)
+# print(solution(6))
+
+a = 50
+print(a // 2)
