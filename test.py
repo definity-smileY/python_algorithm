6313f52639e020e23e960f91fff8191c72e7c1d7
definity-smileY <yysdntjq@naver.com>
2021-07-20 19:25:31 +0900

    TODO update

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import math
-
-def get_pow(N):
-    return int(math.pow(N, 2))
-
-if __name__ == '__main__':
-    a, b, c, d, e = map(int, input().split())
-    print(((get_pow(a)+get_pow(b)+get_pow(c)+get_pow(d)+get_pow(e)) % 10))
+import datetime
+a = datetime.datetime.now()
+# print(a.year)
+# print(a.month)
+# print(a.day)
+a = int(input())
+data = 1946
+print(a - data)
