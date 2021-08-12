251ea0962986f536f3055a1019fbf1c46c689d1c
definity-smileY <yysdntjq@naver.com>
2021-08-12 22:19:41 +0900

    TODO : update

--- 백준/silver2/1105_팔.py
+++ 백준/silver2/1105_팔.py
@@ -12,30 +12,30 @@ else:
                 cnt += 1
     print(cnt)
 
-# a = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+a = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
 
-# size = len(set(a))
-# dic = {a[0]:1}
-# print(dic)
-# temp = [0, len(a) - 1]
-# start , end = 0, 0
+size = len(set(a))
+dic = {a[0]:1}
+print(dic)
+temp = [0, len(a) - 1]
+start , end = 0, 0
 
-# while (start < len(a) and end < len(a)):
-#     if len(dic) == size:
-#         if end - start < temp[1] - temp[0]:
-#             temp = [start, end]
-#         if dic[a[start]] == 1:
-#             del dic[a[start]]
-#         else:
-#             dic[a[start]] -= 1
-#         start += 1
-#     else:
-#         end += 1
-#         if end == len(a):
-#             break
-#         if a[end] in dic.keys():
-#             dic[a[end]] += 1
-#         else:
-#             dic[a[end]] = 1
+while (start < len(a) and end < len(a)):
+    if len(dic) == size:
+        if end - start < temp[1] - temp[0]:
+            temp = [start, end]
+        if dic[a[start]] == 1:
+            del dic[a[start]]
+        else:
+            dic[a[start]] -= 1
+        start += 1
+    else:
+        end += 1
+        if end == len(a):
+            break
+        if a[end] in dic.keys():
+            dic[a[end]] += 1
+        else:
+            dic[a[end]] = 1
 
-# print([temp[0] + 1, temp[1] + 1])
+print([temp[0] + 1, temp[1] + 1])
