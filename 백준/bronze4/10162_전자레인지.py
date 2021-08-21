d88438c0147d8e6c0bd7430749b034899b671eac
definity-smileY <yysdntjq@naver.com>
2021-08-21 23:27:07 +0900

    TODO : update

--- 백준/bronze4/10162_전자레인지.py
+++ 백준/bronze4/10162_전자레인지.py
@@ -1,21 +1,29 @@
 a = int(input())
 
 
-if a % 10 == 0:
-    a_list = [0] * 3
+# if a % 10 == 0:
+#     a_list = [0] * 3
 
-    a_list[0] = a // 300
-    a %= 300
+#     a_list[0] = a // 300
+#     a %= 300
 
-    a_list[1] = a // 60
-    a %= 60
+#     a_list[1] = a // 60
+#     a %= 60
     
-    a_list[2] = a // 10
-    a %= 10
+#     a_list[2] = a // 10
+#     a %= 10
 
-    print(*a_list)
-else:
+#     print(*a_list)
+# else:
+#     print(-1)
+
+
+
+if a % 10 != 0:
     print(-1)
 
-
+else:
+    print(a // 300, end=" ")
+    print((a % 300) // 60, end=" ")
+    print(((a % 300) % 60) // 10)
 
