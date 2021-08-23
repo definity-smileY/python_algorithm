ce7b33b880dd169bfb3c9dfdeca490d6449afa53
definity-smileY <yysdntjq@naver.com>
2021-08-24 00:01:15 +0900

    TODO :update

--- 백준/silver2/1182_부분수열의합.py
+++ 백준/silver2/1182_부분수열의합.py
@@ -5,12 +5,13 @@ n, s = map(int, input().split())
 n_list = list(map(int, input().split()))
 
 count = 0
-for i in range(n):
-    sub = list(combinations(n_list, i))    
+for i in range(1, n+1):
+    sub = list(combinations(n_list, i))
+    # print("sub == ", sub)
     for j in sub:
+        # print("j == ", j)
         if sum(j) == s:
+            # print("sum(j) == 0", sum(j))
             count += 1
 
 print(count)
-
-
