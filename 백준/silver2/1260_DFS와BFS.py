2eb9ca4e854d1717760abc8cc34d116453606f69
definity-smileY <yysdntjq@naver.com>
2021-08-30 23:05:45 +0900

    TODO:update

---
+++ 백준/silver2/1260_DFS와BFS.py
@@ -0,0 +1,35 @@
+import sys
+import collections
+
+input = sys.stdin.readline
+
+def dfs(graph, v):
+    visited = {}
+    stack = [v]
+    
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.setdefault(n)            
+            stack += reversed(graph[n])
+    return visited
+
+n, m, v = map(int, input().split())
+
+graph = {i:[] for i in range(1, n + 1)}
+"""
+{1: [], 2: [], 3: [], 4: []}
+"""
+for i in range(1, m + 1):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+"""
+{1: [2, 3, 4], 2: [1, 4], 3: [1, 4], 4: [1, 2, 3]}
+"""
+
+for key in graph:
+    graph[key].sort()
+
+print(' '.join(list(map(str, dfs(graph, v)))))
+
